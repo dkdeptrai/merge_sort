dbaef2523963cbd0a41c341ec8325c207b92af20
Khoadeptrai <khoa.soft.e@gmail.com>
2022-10-17 23:07:06 +0700

    hihi

--- script.rb
+++ script.rb
@@ -1,5 +1,15 @@
 # frozen_string_literal: true
 
 def merge_sort(arr)
-  arr
+  return arr if arr.length == 1
+
+  left = merge_sort(arr[0...(arr.length / 2)])
+  right = merge_sort(arr[(arr.length / 2)..(arr.length - 1)])
+  merged = []
+  left.first < right.first ? merged << left.shift : merged << right.shift until left.empty? || right.empty?
+  merged + left + right
 end
+
+arr = []
+5_000_000.times { arr << rand(50_000_000) }
+p (merge_sort(arr))
